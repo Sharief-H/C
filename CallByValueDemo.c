0b2bc9cf2887bdbfe2d4092bdb07146209ba3e56
Sharief-H <62561901+Sharief-H@users.noreply.github.com>
2020-04-11 17:09:51 +0530

    Add files via upload

--- CallByValueDemo.c
+++ CallByValueDemo.c
@@ -4,22 +4,21 @@
 #include<stdio.h>
 
 int main()
-{	int n1,n2;
-
- 	printf("\nENTER TWO ELEMENTS FOR SWAPPING \" CALL BY VALUE MECHANISM \": ");
-	scanf("%d%d",&n1,&n2);
- 	printf("\nBEFORE SWAPPING\nNUMBER 1 : %d\nNUMBER 1 : %d",n1,n2);
- 	swap(n1,n2);
-	printf("\n\nIN MAIN AFTER SWAPPING\nNUMBER 1 : %d\nNUMBER 2 : %d",n1,n2);
-
-	return(0);
+{
+    int a, b;
+    a=20; b=30;
+    printf("\nBefore:Number1 is %d and Number2 is %d..",a,b);
+    swap(a,b);
+    printf("\nAfter:Number1 is %d and Number2 is %d..",a,b);
+    return(0);
 }
 
-int swap(int x,int y)
-{	int temp;
-	temp=x;
-	x=y;
-	y=temp;
-	printf("\n\nIN SWAP\nNUMBER 1 : %d\nNUMBER 2 : %d",x,y);
-	return;
+void swap(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("\nNumber1 is %d and Number2 is %d..",a,b);
+
 }
